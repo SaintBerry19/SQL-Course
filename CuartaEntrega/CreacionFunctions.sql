091bb20530125bc54ae7d0c34e4b9a51f26c87e9
SaintBerry <robertomorab.19@gmail.com>
2023-01-04 18:06:11 -0600

    Cuarta Entrega

---
+++ CuartaEntrega/CreacionFunctions.sql
@@ -0,0 +1,42 @@
+USE `ecommerce_sql_course`;
+DROP function IF EXISTS `total_compras`;
+
+DELIMITER $$
+USE `ecommerce_sql_course`$$
+CREATE FUNCTION `total_compras`(numero_compra INT) RETURNS int
+    DETERMINISTIC
+BEGIN
+DECLARE resultado integer;
+SET resultado = (SELECT T.Total_de_compra
+FROM (SELECT c.compra_id, SUM((m.precio*c.cantidad)) as 'Total_de_compra'
+FROM compra_detallada c JOIN materiales m
+ON c.material_id = m.material_id
+GROUP BY compra_id
+ORDER BY compra_id ASC) T
+WHERE T.compra_id = numero_compra);
+RETURN resultado;
+END$$
+
+DELIMITER ;
+
+USE `ecommerce_sql_course`;
+DROP function IF EXISTS `total_venta`;
+
+DELIMITER $$
+USE `ecommerce_sql_course`$$
+CREATE FUNCTION `total_venta`(numero_venta INT) RETURNS int
+    DETERMINISTIC
+BEGIN
+DECLARE resultado integer;
+SET resultado = (SELECT T.Total_de_venta 
+FROM (SELECT v.venta_id, SUM((m.precio*v.cantidad)) as 'Total_de_venta'
+FROM venta_detallada v JOIN materiales m
+ON v.material_id = m.material_id
+GROUP BY venta_id
+ORDER BY venta_id ASC) T
+WHERE T.venta_id = numero_venta);
+RETURN resultado;
+END$$
+
+DELIMITER ;
+
---
+++ CuartaEntrega/CreacionViews.sql
@@ -0,0 +1,68 @@
+CREATE VIEW Venta_Detallada_Explicada
+AS
+(SELECT vd.venta_id,m.nombre,m.precio,vd.cantidad,(m.precio*vd.cantidad) as "Total",v.fecha,T.Nombre_Cliente,T.Nombre_Empleado
+FROM  venta_detallada vd JOIN materiales m
+ON vd.material_id=m.material_id
+JOIN ventas v ON v.venta_id=vd.venta_id
+JOIN (SELECT v.venta_id,CONCAT(c.nombre, " ",c.apellido) AS "Nombre_Cliente",CONCAT(e.nombre, " ",e.apellido) AS "Nombre_Empleado"
+FROM ventas v JOIN clientes c
+ON v.cliente_id=c.cliente_id
+JOIN empleados e
+ON v.empleado_id=e.empleado_id) T
+ON vd.venta_id=T.venta_id
+ORDER BY venta_id);
+
+CREATE VIEW Compra_Detallada_Explicada
+AS
+(SELECT cd.compra_id,m.nombre,m.precio,cd.cantidad,(m.precio*cd.cantidad) as "Total",c.fecha,T.Nombre_Trabajador_Proveedor,T.Nombre_Empleado
+FROM  compra_detallada cd JOIN materiales m
+ON cd.material_id=m.material_id
+JOIN compras c ON c.compra_id=cd.compra_id
+JOIN (SELECT c.compra_id,CONCAT(p.nombre, " ",p.apellido) AS "Nombre_Trabajador_Proveedor",CONCAT(e.nombre, " ",e.apellido) AS "Nombre_Empleado"
+FROM compras c JOIN trabajador_proveedor p
+ON c.trabajador_proveedor_id=p.trabajador_proveedor_id
+JOIN empleados e
+ON c.empleado_id=e.empleado_id) T
+ON cd.compra_id=T.compra_id
+ORDER BY compra_id);
+
+CREATE VIEW Mayores_Vendedores
+AS 
+(SELECT v.empleado_id,COUNT(v.empleado_id) AS "Ventas Totales",CONCAT(e.nombre, " ",e.apellido) AS "Nombre_Empleado",T.Area_de_Trabajo
+FROM ventas v JOIN empleados e
+ON v.empleado_id=e.empleado_id
+JOIN (SELECT e.empleado_id,e.nombre,a.nombre AS "Area_de_Trabajo"
+FROM empleados e JOIN areas a
+ON e.area_id = a.area_id) T
+ON T.empleado_id=v.empleado_id
+GROUP BY empleado_id
+ORDER BY COUNT(empleado_id) DESC);
+
+CREATE VIEW Mayores_Compradores
+AS 
+(SELECT c.empleado_id,COUNT(c.empleado_id) AS "Compras Totales",CONCAT(e.nombre, " ",e.apellido) AS "Nombre_Empleado",T.Area_de_Trabajo
+FROM compras c JOIN empleados e
+ON c.empleado_id=e.empleado_id
+JOIN (SELECT e.empleado_id,e.nombre,a.nombre AS "Area_de_Trabajo"
+FROM empleados e JOIN areas a
+ON e.area_id = a.area_id) T
+ON T.empleado_id=c.empleado_id
+GROUP BY empleado_id
+ORDER BY COUNT(empleado_id) DESC);
+
+CREATE VIEW Mayor_Ingreso_Material
+AS
+(SELECT vd.material_id,m.nombre, SUM(vd.cantidad) AS "Ventas Totales",m.precio, (SUM(vd.cantidad)*m.precio) AS "Total Generado"
+FROM venta_detallada vd JOIN materiales m
+ON vd.material_id=m.material_id
+GROUP BY vd.material_id
+ORDER BY (SUM(vd.cantidad)*m.precio) DESC);
+
+CREATE VIEW Mayor_Egreso_Material
+AS
+(SELECT cd.material_id,m.nombre, SUM(cd.cantidad) AS "Compras Totales",m.precio, (SUM(cd.cantidad)*m.precio) AS "Total Gastado"
+FROM compra_detallada cd JOIN materiales m
+ON cd.material_id=m.material_id
+GROUP BY cd.material_id
+ORDER BY (SUM(cd.cantidad)*m.precio) DESC);
+
